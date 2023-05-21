7d7546c1aa0d012a6f78258bfa6a828fc7e1f06a
mtumalan <mau@tumalan.com>
2023-05-21 14:55:33 -0600

    Se añade personalización de personaje principal dentro de screen PersonalizarMau, así como scripts correspondientes y sprites necesarios

---
+++ Videogames/MealtimeMayhem/Assets/Scripts/CustomizeMaster.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizeMaster : MonoBehaviour
+{
+    private SpriteRenderer head;
+    private SpriteRenderer eyes;
+    private GameObject left;
+    private GameObject right;
+    private GameObject ven;
+    private GameObject usa;
+    private GameObject mex;
+    private VarMaster varMaster;
+    private int nat = 1;
+
+    void Start(){
+        head = GameObject.Find("Head").GetComponent<SpriteRenderer>();
+        eyes = GameObject.Find("Eyes").GetComponent<SpriteRenderer>();
+        ven = GameObject.Find("Venezuela");
+        usa = GameObject.Find("USA");
+        mex = GameObject.Find("México");
+        left = GameObject.Find("Left");
+        right = GameObject.Find("Right");
+        varMaster = GameObject.FindObjectOfType<VarMaster>();
+    }
+
+    void Update(){
+        if (Input.GetMouseButtonDown(0)){
+            BodyColorChanger();
+            EyeColorChanger();
+            NationalityChanger();
+        }
+
+        ven.SetActive(false);
+        usa.SetActive(false);
+        mex.SetActive(false);
+
+        switch (nat){
+            case 1:
+                ven.SetActive(true);
+                break;
+            case 2:
+                mex.SetActive(true);
+                break;
+            case 3:
+                usa.SetActive(true);
+                break;
+        }
+    }
+
+    void BodyColorChanger(){
+        int layerMask = 1 << LayerMask.NameToLayer("UI");
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask);
+        if (hit.collider != null){
+            SpriteRenderer renderer = hit.collider.GetComponent<SpriteRenderer>();
+            if (renderer != null){
+                head.color = renderer.color;
+                varMaster.headColor = head.color;
+            }
+        }
+    }
+
+    void EyeColorChanger(){
+        int layerMask = 1 << LayerMask.NameToLayer("Tiles");
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask);
+        if (hit.collider != null){
+            SpriteRenderer renderer = hit.collider.GetComponent<SpriteRenderer>();
+            if (renderer != null){
+                eyes.color = renderer.color;
+                varMaster.eyeColor = eyes.color;
+            }
+        }
+    }
+
+    void NationalityChanger(){
+        int layerMask = 1 << LayerMask.NameToLayer("Characters");
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask);
+        if (hit.collider != null){
+            if (hit.collider.gameObject.name == "Left"){
+                nat -= 1;
+                varMaster.nat = nat;
+                if (nat < 1){
+                    nat = 3;
+                    varMaster.nat = nat;
+                }
+            }
+            else if (hit.collider.gameObject.name == "Right"){
+                nat += 1;
+                if (nat > 3){
+                    nat = 1;
+                    varMaster.nat = nat;
+                }
+            }
+        }
+    }
+}

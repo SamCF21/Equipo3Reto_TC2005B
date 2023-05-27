77f6122b3cfb26202e0b069db76e1d1f71ad08f1
mtumalan <mau@tumalan.com>
2023-05-27 00:44:19 -0600

    Se termina demo

--- Videogames/MealtimeMayhem/Assets/Scripts/BaseStats.cs
+++ Videogames/MealtimeMayhem/Assets/Scripts/BaseStats.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BaseStats : MonoBehaviour
 {
     public string entityType = "base";
     public int maxhealth = 20;
     public int health = 0;
+    public string winScene;
+    public string loseScene;
 
     void Start()
     {
@@ -18,10 +21,13 @@ public class BaseStats : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
-            Destroy(gameObject);
+            SceneManager.LoadScene(loseScene);
+            if(GameObject.Find("Score").GetComponent<ScoreValue>()){
+                ScoreValue.scoreValue = 0;
+            }
         }
-    } 
-    
+    }   
 
-
+    public void Win(){
+    }
 }

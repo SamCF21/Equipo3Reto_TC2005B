acf0dd68dc0d58c456da22c6ca80727ca70ddadc
Samantha Covarrubias <A01026174@tec.mx>
2023-06-03 15:38:16 -0600

    Overworld (Interactuable con el jugador)

---
+++ Videogames/MealtimeMayhem/Assets/Scripts/MoverPersonaje.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class MoverPersonaje : MonoBehaviour
+{
+    public float velocidadMovimiento = 5f; // Velocidad de movimiento del personaje
+
+    private Vector2 destino; // Destino del personaje
+    private bool isMoving; // Indica si el personaje está en movimiento
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // Obtener la posición del clic en la pantalla
+            Vector2 posicionClic = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // Establecer el destino del personaje
+            destino = posicionClic;
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            // Calcular la dirección hacia el destino
+            Vector2 direccion = (destino - (Vector2)transform.position).normalized;
+
+            // Mover el personaje hacia el destino
+            transform.Translate(direccion * velocidadMovimiento * Time.deltaTime);
+
+            // Verificar si ha llegado al destino
+            if (Vector2.Distance(transform.position, destino) < 0.1f)
+            {
+                isMoving = false;
+            }
+        }
+    }
+}
+
+

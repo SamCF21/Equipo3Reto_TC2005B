834b3c2b78c750dbdb66819655d474081b8fd83b
mtumalan <mau@tumalan.com>
2023-06-03 23:44:09 -0600

    Se añade outline para personajes al seleccionarlos

--- Videogames/MealtimeMayhem/Assets/Scripts/Allies/AllyMovement.cs
+++ Videogames/MealtimeMayhem/Assets/Scripts/Allies/AllyMovement.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class AllyMovement : MonoBehaviour
 {
     public Transform target;
-    public float speed;
+    public bool isSelected;
+    [SerializeField] float speed;
+    [SerializeField] Material selectedMaterial;
+    private Material actualMaterial;
     private SpriteRenderer spriteRenderer;
     private MainStats mainStats;
     private ClassStats classStats;
-    public bool timeDelay;
 
     void Start(){
         spriteRenderer = GetComponent<SpriteRenderer>();
+        actualMaterial = spriteRenderer.material;
         classStats = GetComponent<ClassStats>();
         mainStats = GameObject.FindObjectOfType<MainStats>();
-        timeDelay = false;
+        isSelected = false;
     }
 
     void Update(){
+        MovePos();
+        Selection();
+    }
+
+    void MovePos(){
         speed = classStats.speed * mainStats.globalSpeed;
         if (target != null){
         float actPos = transform.position.x;
@@ -29,6 +37,14 @@ public class AllyMovement : MonoBehaviour
             if(actPos < transform.position.x){
                 spriteRenderer.flipX = false;
             }
+        }
+    }
+
+    void Selection(){
+        if(isSelected){
+            spriteRenderer.material = selectedMaterial;
+        }else if (!isSelected){
+            spriteRenderer.material = actualMaterial;
         }
     }
 }
--- Videogames/MealtimeMayhem/Assets/Scripts/Allies/AllyMovementMaster.cs
+++ Videogames/MealtimeMayhem/Assets/Scripts/Allies/AllyMovementMaster.cs
@@ -2,42 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class AllyMovementMaster : MonoBehaviour
 {
-    private Transform target;
-    private Transform ally;
+    private GameObject target; 
+    private GameObject ally;
+    private AllyMovement selectedAlly;
+    private AllyMovement prevAlly;
     public MainStats mainStats;
 
-    void Start(){
+    void Start()
+    {
         mainStats = GameObject.FindObjectOfType<MainStats>();
     }
 
-    void Update(){
-        if (Input.GetMouseButtonDown(0)){
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             int layerMask = 1 << LayerMask.NameToLayer("Characters");
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask);
-            if (hit.collider != null){
-                if (hit.collider.CompareTag("Ally")){
-                    ally = hit.collider.transform;
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("Ally"))
+                {
+                    ally = hit.collider.gameObject;
+                    selectedAlly = ally.GetComponent<AllyMovement>();
+                    if(prevAlly != null){
+                        prevAlly.isSelected = false;
+                    }
+                    if(selectedAlly != null){
+                        selectedAlly.isSelected = true;
+                        prevAlly = selectedAlly;
+                    } 
                 }
             }
         }
 
-        if (ally != null){
-            if (Input.GetMouseButtonDown(0)){
+        if (ally != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
                 int layerMask2 = 1 << LayerMask.NameToLayer("Tiles");
                 RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask2);
-                if (hit.collider != null && hit.collider != ally.GetComponent<Collider>()){
-                    target = hit.transform;
+                if (hit.collider != null && hit.collider.gameObject != ally) 
+                {
+                    target = hit.collider.gameObject; 
                 }
             }
         }
 
-        if (target != null && ally != null){
-            AllyMovement movement = ally.GetComponent<AllyMovement>();
-            if (movement != null){
-                movement.target = target;
+        if (target != null && ally != null)
+        {
+            selectedAlly = ally.GetComponent<AllyMovement>();
+            if (selectedAlly != null)
+            {
+                selectedAlly.target = target.transform; 
             }
         }
     }

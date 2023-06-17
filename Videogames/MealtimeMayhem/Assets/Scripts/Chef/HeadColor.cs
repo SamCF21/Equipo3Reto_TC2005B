37e75f0da9c6e5780a54615cc418a5a504a859b6
Cristina <A01025667@tec.mx>
2023-06-16 23:58:44 -0600

    Se agregó música

--- Videogames/MealtimeMayhem/Assets/Scripts/Chef/HeadColor.cs
+++ Videogames/MealtimeMayhem/Assets/Scripts/Chef/HeadColor.cs
@@ -7,13 +7,46 @@ public class HeadColor : MonoBehaviour
     private VarMaster varMaster;
     private SpriteRenderer spriteRenderer;
 
+    
     void Awake()
     {
         varMaster = FindObjectOfType<VarMaster>();
         if (varMaster != null)
         {
             spriteRenderer = GetComponent<SpriteRenderer>();
-            spriteRenderer.color = varMaster.headColor;
+
+            
+            switch (varMaster.codeHead)
+            {
+                case 1: // Rojo
+                    spriteRenderer.color = Color.red;
+                    break;
+                case 2: // Naranja
+                    spriteRenderer.color = new Color(1f, 0.647f, 0f);
+                    break;
+                case 3: // Amarillo
+                    spriteRenderer.color = Color.yellow;
+                    break;
+                case 4: // Verde
+                    spriteRenderer.color = Color.green;
+                    break;
+                case 5: // Azul
+                    spriteRenderer.color = Color.blue;
+                    break;
+                case 6: // Morado
+                    spriteRenderer.color = new Color(0.502f, 0f, 0.502f);
+                    break;
+                case 7: // Negro
+                    spriteRenderer.color = Color.black;
+                    break;
+                case 8: // Blanco
+                    spriteRenderer.color = Color.white;
+                    break;
+                default:
+                    spriteRenderer.color = Color.white; // Default color 
+                    break;
+            }
         }
     }
+
 }
